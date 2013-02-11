bf5156050e28942bd4376cd91ce126352b77bb62
her0satr <her0satr@yahoo.com>
2013-02-11 01:42:40 -0800

    Update DB

--- godfather_db.sql
+++ godfather_db.sql
@@ -3,11 +3,17 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Waktu pembuatan: 11. Februari 2013 jam 07:09
--- Versi Server: 5.1.41
--- Versi PHP: 5.3.1
+-- Generation Time: Feb 11, 2013 at 04:41 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `godfather_db`
@@ -16,7 +22,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `city`
+-- Table structure for table `city`
 --
 
 CREATE TABLE IF NOT EXISTS `city` (
@@ -29,42 +35,42 @@ CREATE TABLE IF NOT EXISTS `city` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
 
 --
--- Dumping data untuk tabel `city`
+-- Dumping data for table `city`
 --
 
 INSERT INTO `city` (`city_id`, `user_id`, `city`, `city_time`, `city_desc`) VALUES
 (1, 2, 'First', '2013-02-12 08:08:42', '5 Production 20 City = 559 | 455'),
 (2, 2, 'Greenwich', '2013-02-11 23:02:01', 'Truck'),
-(3, 2, 'Brooklyn', '2013-02-11 10:03:19', ''),
-(4, 3, 'First', '2013-02-11 09:29:42', ''),
-(5, 3, 'Greenwich', '2013-02-11 15:20:12', ''),
-(6, 4, 'First', '2013-02-12 00:15:27', ''),
+(3, 2, 'Brooklyn', '2013-02-12 16:20:21', ''),
+(4, 3, 'First', '2013-02-12 09:38:49', ''),
+(5, 3, 'Greenwich', '2013-02-12 02:06:57', ''),
+(6, 4, 'First', '2013-02-12 00:15:27', 'Truck'),
 (7, 5, 'First', '2013-02-14 00:44:15', ''),
-(8, 6, 'First', '2013-02-11 07:35:25', ''),
-(9, 7, 'First', '2013-02-11 16:28:18', ''),
-(10, 8, 'First', '2013-02-11 16:52:33', ''),
-(11, 8, 'Greenwich', '2013-02-11 08:51:11', ''),
-(12, 4, 'Greenwich', '2013-02-11 10:12:32', ''),
-(13, 2, 'Park', '2013-02-11 13:24:31', ''),
-(14, 3, 'Brooklyn', '2013-02-11 10:35:03', ''),
+(8, 6, 'First', '2013-02-12 06:11:27', ''),
+(9, 7, 'First', '2013-02-12 02:59:46', ''),
+(10, 8, 'First', '2013-02-11 20:40:31', ''),
+(11, 8, 'Greenwich', '2013-02-11 20:40:45', ''),
+(12, 4, 'Greenwich', '2013-02-13 01:55:34', ''),
+(13, 2, 'Park', '2013-02-11 22:50:54', ''),
+(14, 3, 'Brooklyn', '2013-02-11 17:27:12', ''),
 (15, 3, 'Park', '2013-02-11 21:38:37', ''),
-(16, 4, 'Brooklyn', '2013-02-11 10:42:04', ''),
-(17, 6, 'Greenwich', '2013-02-11 08:17:25', ''),
-(18, 6, 'Brooklyn', '2013-02-11 08:47:34', ''),
-(19, 6, 'Park', '2013-02-11 09:50:00', ''),
-(20, 2, 'Atlantic', '2013-02-11 11:45:14', ''),
-(21, 5, 'Greenwich', '2013-02-11 07:49:42', ''),
-(22, 5, 'Brooklyn', '2013-02-11 07:49:57', ''),
-(23, 7, 'Greenwich', '2013-02-11 13:51:51', ''),
-(24, 8, 'Park', '2013-02-11 10:53:05', ''),
-(25, 4, 'Park', '2013-02-11 11:25:47', ''),
-(26, 3, 'Atlantic', '2013-02-11 10:49:17', ''),
-(27, 4, 'Atlantic', '2013-02-11 07:42:50', '');
+(16, 4, 'Brooklyn', '2013-02-11 18:05:43', ''),
+(17, 6, 'Greenwich', '2013-02-11 18:13:06', ''),
+(18, 6, 'Brooklyn', '2013-02-11 18:13:08', ''),
+(19, 6, 'Park', '2013-02-11 16:48:10', ''),
+(20, 2, 'Atlantic', '2013-02-11 20:21:22', ''),
+(21, 5, 'Greenwich', '2013-02-11 17:07:38', ''),
+(22, 5, 'Brooklyn', '2013-02-11 17:07:55', ''),
+(23, 7, 'Greenwich', '2013-02-12 08:08:58', ''),
+(24, 8, 'Park', '2013-02-11 20:40:54', ''),
+(25, 4, 'Park', '2013-02-11 20:26:25', ''),
+(26, 3, 'Atlantic', '2013-02-11 17:07:44', ''),
+(27, 4, 'Atlantic', '2013-02-11 20:25:19', '');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
@@ -76,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `user_email`, `user_pass`, `user_display`) VALUES
@@ -87,3 +93,7 @@ INSERT INTO `user` (`user_id`, `user_email`, `user_pass`, `user_display`) VALUES
 (6, 'bungalowq@ymail.com', '4532&^145', 'Bunga Low'),
 (7, 'makinbunawas@yahoo.com', 'jasd%^521aa', 'Bunawas'),
 (8, 'dunkomp@gmail.com', 'qwertyui', 'Don Comp');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
