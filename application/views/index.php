0c9c470c26d490bccb0c4857a758272fb42ef31b
her0satr <her0satr@yahoo.com>
2013-02-18 16:53:10 -0800

    Update Refresh per 2 Minute

--- application/views/index.php
+++ application/views/index.php
@@ -3,6 +3,7 @@
 ?>
 <head>
 	<title>God Father</title>
+	<meta http-equiv="refresh" content="120">
 	<script type="text/javascript">var Host = { Link: '<?php echo $this->config->item('base_url'); ?>' }</script>
 	<script type="text/javascript" src="static/jquery/jquery.min.js"></script>
 	<style>
