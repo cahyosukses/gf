08822d3bc89f22c1fdb4f774ba4f7180b823d9cc
Herry Satrio <her0satr@yahoo.com>
2015-08-10 15:11:44 +0700

    update

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -19,17 +19,20 @@
 	<div>20 Week for idle town</div>
 	<div>5 Production 49 City = 559 | 455</div>
 	<div>1500 min Black Widow for all town</div>
-	<div style="padding: 0 0 25px 0;">Penthouse Box max 100, power lebih utama</div>
+	<div style="padding: 0 0 25px 0;">Penthouse Box max 175, power lebih utama</div>
 	<div style="padding: 0 0 25px 0;">Murder Inc Lv 1<br />700 Assassin + 70 Black Widow + 70 Transport</div>
 	<div style="padding: 0 0 25px 0;">Murder Inc Lv 2<br />10000 DRC + 1000 Crooked Cop + 150 Thug + 500 Transport</div>
-	<div>Murder Inc Lv 3<br />10000 DRC + 1000 Crooked Cop + 200 Thug + 500 Transport</div>
-	<div style="padding: 10px 0 0 0;">
+	<div style="padding: 0 0 25px 0;">Murder Inc Lv 3<br />10000 DRC + 1000 Crooked Cop + 200 Thug + 500 Transport</div>
+	<div style="padding: 0 0 25px 0;">
 		Attack ISB<br />
 		Daily Token<br />
 		Daily Doria<br />
 		Doria Random<br />
 		Council<br />
 		Build<br />
+	</div>
+	<div style="padding: 0 0 25px 0;">
+		Crew Ranking #16 - 1.000.000
 	</div>
 </div>
 
